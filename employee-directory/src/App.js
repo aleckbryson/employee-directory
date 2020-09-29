a545084ab2564af5875493abc24b6247d688a7e5
Aleck Tuluenga <aleckbryson@gmail.com>
2020-09-29 00:11:14 -0400

    Sort handle function added to sort pay bands

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -4,33 +4,42 @@ import employee from './employees.json'
 
 class App extends React.Component {
   state = {
-    employeeList: employee
+    employeeList: employee,
+    sortOrder: " "
   }
+
+  handleSort = () => {
+    const sortList = this.state.employeeList.sort(function (a, b) {
+      return a.payBand - b.payBand;
+    });
+    this.setState({employeeList: sortList})
+  }
+  
   render() {
-    return (
+      return(
       <>
-        <table className="table">
-          <thead className="thead-dark">
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">First Name</th>
-              <th scope="col">Last Name</th>
-              <th scope="col">Title</th>
-              <th scope="col">Pay Band</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.employeeList.map(employee => (
-              <tr>
-                <th scope="row">{employee.id}</th>
-                <td>{employee.firstName}</td>
-                <td>{employee.lastName}</td>
-                <td>{employee.title}</td>
-                <td>{employee.payBand}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <table className="table">
+      <thead className="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Title</th>
+          <th scope="col" onClick={() => this.handleSort()}>Pay Band</th>
+        </tr>
+      </thead>
+      <tbody>
+        {this.state.employeeList.map(employee => (
+          <tr>
+            <th scope="row">{employee.id}</th>
+            <td>{employee.firstName}</td>
+            <td>{employee.lastName}</td>
+            <td>{employee.title}</td>
+            <td>{employee.payBand}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
       </>
     );
   }
