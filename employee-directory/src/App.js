f6047bc3a40351cb0bddd1b30c73ea0e381e6089
Aleck Tuluenga <aleckbryson@gmail.com>
2020-09-29 01:11:43 -0400

    Sort handle function added for names and onclicks added to the title headers

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -9,11 +9,54 @@ class App extends React.Component {
   }
 
   handleSort = () => {
-    const sortList = this.state.employeeList.sort(function (a, b) {
-      return a.payBand - b.payBand;
+    const sortList = this.state.employeeList.sort((a, b) => {
+      if (this.state.sortOrder !== "des") {
+        return a.payBand - b.payBand;
+      } else {
+        return b.payBand - a.payBand;
+      }
     });
-    this.setState({employeeList: sortList})
+    const newSort = this.state.sortOrder === "des" ? "asc" : "des"
+    this.setState({ employeeList: sortList, sortOrder: newSort })
   }
+
+  // handleSortId = () => {
+  //   const sortId = this.state.employeeList.sort((a, b) => {
+  //     if (this.state.sortOrder !== "des") {
+  //       return a.id - b.id;
+  //     } else {
+  //       return b.id - a.id;
+  //     }
+  //   });
+  //   const newSortId = this.state.sortOrder === "des" ? "asc" : "des"
+  //   this.setState({ employeeList: sortId, sortOrder: newSortId })
+  // }
+
+  handleName = () => {
+    const sortName = this.state.employeeList.sort((a, b) => {
+      let nameA = a.firstName.toUpperCase();
+      let nameB = b.lastName.toLowerCase();
+      if (this.state.sortOrder !== "des") {
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0
+    } else { 
+      if (nameA < nameB) {
+        return 1;
+      }
+      if (nameA > nameB) {
+        return -1;
+      }
+      return 0;
+    }
+  })
+  const newSort = this.state.sortOrder === "des" ? "asc" : "des"
+  this.setState({ employeeList: sortName, sortOrder: newSort })
+}
   
   render() {
       return(
@@ -22,8 +65,8 @@ class App extends React.Component {
       <thead className="thead-dark">
         <tr>
           <th scope="col">#</th>
-          <th scope="col">First Name</th>
-          <th scope="col">Last Name</th>
+          <th scope="col" onClick={() => this.handleName()}>First Name</th>
+          <th scope="col" onClick={() => this.handleName()}>Last Name</th>
           <th scope="col">Title</th>
           <th scope="col" onClick={() => this.handleSort()}>Pay Band</th>
         </tr>
